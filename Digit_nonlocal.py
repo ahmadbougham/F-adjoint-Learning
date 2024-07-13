643061067fbee89fd051cefda91a2dd6f831ce56
ahmadbougham <135628227+ahmadbougham@users.noreply.github.com>
2024-07-13 11:15:21 +0100

    Update Digit_nonlocal.py

--- Digit_nonlocal.py
+++ Digit_nonlocal.py
@@ -167,14 +167,8 @@ class MultiLayerPerceptron:
     (Ystar, Xstar)=self.Fstar_pass(x, y)    
     #######################################################################
     for l in reversed(range(1, self.num_layers+1)):        
-        self.w[l] = self.w[l] - self.alpha *  np.dot(Ystar[l], Xb[l- 1].T)  
-        reg_matrix_w = np.zeros_like(self.w[l])
-        if self.regg == 1:
-          self.w[l]=self.w[l]+ (self.lambd) * np.sign(self.w[l])
-          pass
-        elif self.regg == 2:
-          self.w[l]= self.w[l]+ 2 * (self.lambd) * self.w[l] 
-    ############################       
+        self.w[l] = self.w[l] - self.alpha *  np.dot(Ystar[l], Xb[l- 1].T)          
+    ##########################################################################       
 
   def predict(self, x):
     """
@@ -186,33 +180,19 @@ class MultiLayerPerceptron:
   def evaluate_acc(self, yhat, y):  
     return np.mean(yhat == y)
    
-  def fit(self, x_train, y_train, epochs, mini_batch_size, alpha, x_test, y_test, regularization = 0, lambd = 0):
+  def fit(self, x_train, y_train, epochs, mini_batch_size, alpha, x_test, y_test):
       """
-      main function used to fit the model  
+      Main function used to fit the model  
       """
       self.alpha = alpha
       self.loss = Cross_Entropy(self.activations[self.num_layers])#Cross_Entropy
-      # self.lambd = regularization() 
-      if regularization == 0:
-       # print("No Reg")
-        self.regg = 0
-        self.lambd = 0
-      elif regularization == 1:      ##Doing L1 Regularization
-        print("L1 Reg")
-        self.regg = 1
-        self.lambd = lambd
-
-      else: #Do L2 Regularization       #Regularization L2
-        print("L2 Reg")
-        self.regg = 2
-        self.lambd = lambd
-      #Setting up some  parameters for graphing!
+      # Setting up some  parameters for graphing!
       self.train_logger = []
       self.test_logger = []
       self.cost_logger = []
 
       for i in range(epochs):
-        #Randomizing the data:
+        # Randomizing the data:
         m=x_train.shape[1]
         permutation = np.random.permutation(m)
         shuffled_X = x_train[:,permutation]
@@ -236,7 +216,7 @@ class MultiLayerPerceptron:
         # print results for monitoring while training
         print("Epoch {0} train data: {1} %".format(i, 100 * (training_acc)))
         print("Epoch {0} test data: {1} %".format(i, 100 * (testing_acc)))    
-
+############################################################################################
 # To plot figure
 def plot_fig(data):
     fig,ax = plt.subplots(1,1,figsize=(9,7))
@@ -260,14 +240,14 @@ def plot_fig(data):
     fig.savefig("Digit_nonlocal.png", format="png")   
     plt.show()
     plt.close()
-    ####################################################################
+###########################################################################################
 if __name__ == "__main__":
     
     import math
     np.random.seed(4) 
   # create model
     MLP_xavier = MultiLayerPerceptron([784,  128, 10], [Sigmoid_Class ,Sigmoid_Class], weight = "xavier")
-    MLP_xavier.fit(x_train, y_train, 10, 128, 0.001, x_test, y_test, regularization = 0, lambd = 0)
+    MLP_xavier.fit(x_train, y_train, 10, 128, 0.001, x_test, y_test)
     # Plot data
     data = [("Training", MLP_xavier.train_logger),("Testing", MLP_xavier.test_logger)]    
     plot_fig(data)
